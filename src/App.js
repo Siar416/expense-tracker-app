a5233e82303366ca160551862e2aed6fb2b3d417
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-08 14:38:03 -0400

    Created UI by creating balance, income expense, transation and add transation components, along with adding state and on change function to form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 
 function App() {
   return (
     <h2>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </h2>
   );
 }
