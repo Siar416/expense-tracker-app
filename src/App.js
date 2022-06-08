ea8530394cf83b00b5aa0adfd89ffe8d8909c06c
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-08 14:17:55 -0400

    Created components folder. Created Header file and added global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <h2>
+      <Header />
+    </h2>
+  );
 }
 
 export default App;
