74b3d81bca0e2c26567f94dc589ba7ae340394a3
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-08 15:43:58 -0400

    Created action to delete transaction. Created case for delete in app reducer. Added delete functionality in transaction component

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -7,8 +7,6 @@ export const Balance = () => {
   const amount = transactions.map((transaction) => transaction.amount);
   const total = amount.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
-  console.log(total);
-
   return (
     <>
       <h4>Your Balance</h4>
